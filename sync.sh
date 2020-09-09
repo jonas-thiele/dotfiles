afde94b8257a41cf6eb88e03d3538bd4443c0bd1
Jonas Thiele <JonasThi97@gmail.com>
2020-09-10 00:49:35 +0200

    Use symlinks to synchronize symlinks

--- sync.sh
+++ sync.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 
+find_dotfiles() {
+    find -H "$HOME/dotfiles/" -name '*.sync'
+}
+
+sync () {
+    for file in $(find_dotfiles); do
+        destination="$HOME/.$(basename "$file" '.sync')"
+        if [ -e "$destination" ]; then
+            echo "    $destination already exists!"
+        else
+            echo "    $destination -> $file"
+            ln -s "$file" "$destination"
+        fi
+    done
+}
+
 echo "Pulling dotfiles repository"
-git -C ~/dotfiles/ pull origin master
+git -C "$HOME/dotfiles/" pull origin master
 
-echo "Copying dotfiles into home directory"
-rsync --exclude ".git/" --exclude "sync.sh" --exclude "*.swp" --exclude "*.swo" -avh --no-perms ~/dotfiles/ ~
-
-echo "Reloading bashrc"
-source ~/.bashrc
+echo "Creating symlinks for dotfiles"
+sync
